cb931b6991028af65980d172e6b874ad2b715c54
김준원 <junis3@snu.ac.kr>
2020-12-22 22:24:05 +0900

    Built client structure!

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div className='title'>
+                Team WoodenCompass
+            </div>
+        </header>
+    )
+}
+
+export default Header;
