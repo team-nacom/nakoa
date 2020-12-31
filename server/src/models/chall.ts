665dc353bbcf21353c42696008b3ae45c66e34e5
diuven <9127047+Diuven@users.noreply.github.com>
2020-12-31 22:57:02 +0900

    Improved /chall API

--- server/src/models/chall.ts
+++ server/src/models/chall.ts
@@ -1,9 +1,13 @@
 import { model, Schema } from "mongoose";
 
 const challSchema = new Schema({
+    // _id: Number (default)
     index: Number,
     name: String,
+    isPublic: { type: Boolean, default: false },
     createDate: { type: Date, default: Date.now }
 });
+
+// challSchema.methods.getStatemetPdfLocation
 
 export default model('Chall', challSchema);
