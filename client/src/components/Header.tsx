d1d03e99702b9469d8ba24a372098164ab0ecb53
김준원 <junis3@snu.ac.kr>
2021-01-03 21:57:20 +0900

    Improved Header UI

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,21 +1,27 @@
+import { mainColor, subColor } from 'etc/consts';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header>
-            <Link to='/'>
-                <div className='title'>
-                    Team WoodenCompass
-                </div>
-            </Link>
-            <ul className='menu'>
-                <Link to='/challenge'> 
-                    <li>
-                        Challenge
-                    </li>
+            <div className='navbar'>
+                <Link to='/'>
+                    <div className='title'>
+                        <span style={{color: mainColor}}>나무</span><span style={{color: subColor}}>컴퍼스</span>
+                    </div>
                 </Link>
-            </ul>
+                <ul className='menu'>
+                    <Link to='/challenge'> 
+                        <li>
+                            Challenge
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+            <div className='menubar'>
+                로그인
+            </div>
         </header>
     )
 }
