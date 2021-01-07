90186490fdfceda5da91d67ebbe993275486e9d4
diuven <9127047+Diuven@users.noreply.github.com>
2021-01-07 22:47:42 +0900

    Added AWS S3 connection

---
+++ server/src/models/aws.ts
@@ -0,0 +1,24 @@
+// Import required AWS SDK clients and commands for Node.js
+import { S3Client, PutObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
+
+// Set the AWS region
+const REGION = "ap-northeast-2"; // SEOUL
+const BUCKET = "nacom-dev-storage";
+
+// Create an S3 client service object
+const s3 = new S3Client({ region: REGION });
+
+const initialRun = async () => {
+  console.log(`Trying to connect to ${BUCKET}...`);
+  try {
+    const data = await s3.send(new ListObjectsCommand({
+      Bucket: BUCKET,
+    }));
+    console.log("Initial S3 connection successful!");
+  } catch (err) {
+    console.error("Error on initial S3 connection");
+    throw err;
+  }
+};
+
+initialRun();
