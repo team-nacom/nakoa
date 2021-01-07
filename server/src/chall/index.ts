7539ae68e5243d9324331849fdb959f6afc26840
diuven <9127047+Diuven@users.noreply.github.com>
2021-01-07 23:15:12 +0900

    Added /chall/:id request

--- server/src/chall/index.ts
+++ server/src/chall/index.ts
@@ -1,18 +1,37 @@
 import Router from 'koa-router';
+import { idText } from 'typescript';
 
 import Chall from '../models/chall';
 
 const router = new Router();
 
+// NOTE: exclude _id?
 router.get('/', async (ctx) => {
   try {
     ctx.body = await Chall.find(
       { isPublic: true }, // return only public challs
       'index name'        // project index & name fields only
     ).exec();
-  } catch (e) {
-    return ctx.throw(500, e);
+  } catch (err) {
+    return ctx.throw(500, err);
   }
 });
+
+router.get('/:index', async (ctx, next) => {
+  const index = ctx.params.index;
+  try {
+    ctx.body = await Chall.find(
+      {
+        index: index,
+        isPublic: true,
+      },
+      '-isPublic'
+    ).exec();
+  } catch (err) {
+    return ctx.throw(500, err);
+  }
+  next();
+});
+
 
 export default router;
