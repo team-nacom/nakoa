e8159a3ba4f8b1c99a83295925d0ca7908dcedc9
김준원 <junis3@snu.ac.kr>
2021-01-07 22:21:50 +0900

    Added signup page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -8,7 +8,20 @@ function SignUp() {
             <Header/>
             <h2> 가입 </h2>
             <p> 나무컴퍼스에 관심을 가지고 가입해주셔서 감사합니다. </p>
+            <p> 가입하시려면, 아래 항목을 채워주세요. 입력해주신 개인정보는 로그인 외 다른 용도로 이용되지 않습니다.</p>
 
+            <div className='signupBox'>
+                <div className='signupLabel'> 이메일 (아이디) </div>
+                <input className='signupForm' placeholder='예시: example@gmail.com'/>
+
+                <div className='signupLabel'> 비밀번호 </div>
+                <input type='password' className='signupForm' placeholder='8글자 이상 영문+숫자'/>
+
+                <div className='signupLabel'> 비밀번호 확인 </div>
+                <input type='password' className='signupForm'/>
+            </div>
+
+            <button className='button'> 가입하기 </button>
             <Footer/>
         </>
     )
