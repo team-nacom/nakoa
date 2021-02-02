0fbc261ab41724ebb0c70ea796a2ca12b0b44435
diuven <9127047+Diuven@users.noreply.github.com>
2021-02-02 22:13:45 +0900

    Added user register

--- server/src/user/index.ts
+++ server/src/user/index.ts
@@ -1,9 +1,28 @@
 import Router from 'koa-router';
 import passport from 'koa-passport';
 
+import User from '../models/user';
+
 const router = new Router();
 
 router.use(passport.session());
+
+router.post('/register', async (ctx) => {
+  const userObj = ctx.request.body;
+
+  if(!userObj || !userObj.name || !userObj.password) {
+    console.error(`User is ill-formed ${userObj}`);
+    ctx.throw(400);
+  } else if(await User.exists({ name: userObj.name })) {
+    console.error(`User with username ${userObj.name} already exists`);
+    ctx.throw(400);
+  } else {
+    const user = new User(userObj);
+    await user.save()
+      .then(doc => { console.log(`New user ${userObj.name} successfully created`); ctx.redirect('/user/check'); })
+      .catch(err => { console.error(err); ctx.throw(500); });
+  }
+})
 
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/user/check',
