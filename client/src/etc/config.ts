f61e8b8efebea6651589b4180a2a2cf8835d859b
Nacom Manager <nacommanager@gmail.com>
2021-03-04 12:43:38 +0000

    Changed API address

--- client/src/etc/config.ts
+++ client/src/etc/config.ts
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    apiAddress: process.env.API_ADDRESS || 'http://localhost:3885',
+    apiAddress: process.env.API_ADDRESS || 'http://52.78.238.149:3885',
 }
