c6be551170801c97567f797c4bd142720724287d
paulsohn <paulsohn@outlook.kr>
2021-03-25 14:49:09 +0000

    Add responsive preview button (todo: stylesheet)

--- client/src/components/MarkdownEditor.tsx
+++ client/src/components/MarkdownEditor.tsx
@@ -5,56 +5,74 @@ import { useMediaQuery } from 'react-responsive';
 
 import MarkdownRenderer from './MarkdownRenderer';
 
-const MarkdownArea = styled.textarea`
-    width: 100% !important;
-    height: 300px !important;
-    overflow-y: scroll;
-    box-sizing:border-box;
+function MarkdownArea(props : React.TextareaHTMLAttributes<HTMLTextAreaElement>){
+    return(
+        <textarea {...props} />
+        // className={ (props.className || '') + ' markdownArea' }
+    )
+}
 
-    padding: 4px;
+function PreviewArea(props : React.HTMLAttributes<HTMLDivElement>){
+    return(
+        <div {...props} />
+        // className={ (props.className || '') + ' previewArea' }
+    )
+}
 
-    resize: none;
-`
-
-const PreviewArea = styled.div`
-    width: 100%;
-    height: 300px;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    box-sizing:border-box;
-
-    margin: 10px 0px;
-    padding: 4px 10px;
-
-    border: 1px solid #888888;
-`
-
-interface PanelProps extends React.HTMLAttributes<HTMLDivElement>{
-    count: number;
+interface PanelProps extends React.HTMLAttributes<HTMLElement>{
+    collapse: boolean;
     activeIndex: number | string;
     index: number | string;
 }
 
-function Panel({children, count, activeIndex, index, ...other} : PanelProps){
-    const isMobile = useMediaQuery({ query: `(max-width:768px)` });
-
+function Panel({children, collapse, activeIndex, index, ...other} : PanelProps){
     return(
         <div style={ {
             float: 'left',
-            width: isMobile? '100%' : `calc(100% / ${ count.toString() })`
-        } } hidden={ isMobile && activeIndex !== index }>
+            width: collapse? '100%' : '50%',
+            display: (collapse && activeIndex !== index) ? 'none' : 'flex'
+        } }>
             { children }
         </div>
     )
 }
 
-interface EditorProps extends React.HTMLAttributes<HTMLTextAreaElement>{
-    body?: string;
-    update: (c : string) => void; //can we do this w/o callback?
+interface PanelMenuProps extends React.HTMLAttributes<HTMLElement>{
+    collapse: boolean;
+    activeIndex: number | string;
+    index: number | string;
+    callback: (newActiveIndex : number | string) => void;
 }
 
-function MarkdownEditor({ body, update, ...other } : EditorProps) {
+function PanelMenu({children, collapse, activeIndex, index, callback, ...other} : PanelMenuProps){
+    return(
+        <>
+            <div className = 'panelMenuWrapper' style={ {
+                float: 'left',
+                width: collapse? 'auto' : '50%',
+            } }>
+                <button
+                className = { 'panelMenu' + ((collapse && activeIndex === index) ? ' selected' : '') }
+                onClick = { (e)=> { callback(index) } }
+                disabled = { !collapse }>
+                    { children }
+                </button>
+            </div>
+        </>
+    )
+}
+
+interface EditorProps extends React.HTMLAttributes<HTMLTextAreaElement>{
+    body?: string;
+    collapse?: boolean;
+    update: (c : string) => void //can we do this w/o callback?
+}
+
+function MarkdownEditor({ body, collapse, update, ...other } : EditorProps) {
     const [value,setValue] = useState(body || '');
+    const [activeIndex,setActiveIndex] = useState(1 as number | string);
+    let _collapse = useMediaQuery({ query: `(max-width:768px)` }) || collapse || false;
+    // collapse priority: mobile true > argument > default false(i.e. parallel)
 
     const innerUpdate = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
         setValue(e.target.value);
@@ -63,11 +81,13 @@ function MarkdownEditor({ body, update, ...other } : EditorProps) {
 
     return (
         <>
-            <Panel count={2} activeIndex={1} index={1} >
-                <MarkdownArea {...other} placeholder='Markdown 및 LaTeX 수식 입력 가능' onChange={ innerUpdate } value={ value } />
+            <PanelMenu collapse = { _collapse } activeIndex={ activeIndex } index={1} callback = { setActiveIndex }>편집</PanelMenu>
+            <PanelMenu collapse = { _collapse } activeIndex={ activeIndex } index={2} callback = { setActiveIndex }>미리보기</PanelMenu>
+            <Panel collapse = { _collapse } activeIndex={ activeIndex } index={1} >
+                <MarkdownArea {...other} className={ `${other.className || ''} markdownArea` } placeholder='Markdown 및 LaTeX 수식 입력 가능' onChange={ innerUpdate } value = { value } />
             </Panel>
-            <Panel count={2} style={ { float: 'right'} } activeIndex={1} index={2}>
-                <PreviewArea className='blog'>
+            <Panel collapse = { _collapse } style={ { float: 'right'} } activeIndex={ activeIndex } index={2}>
+                <PreviewArea className='blog previewArea'>
                     <MarkdownRenderer style={ {padding: '4px'} } source={ value } />
                 </PreviewArea>
             </Panel>
