1540b454b68a8567ddf9f46f6808260af73e2bd6
paulsohn <paulsohn@outlook.kr>
2021-03-29 14:10:45 +0000

    (minor) li should be direct children of ul

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -27,41 +27,30 @@ function Header() {
                                 { user.nickname + '님, 안녕하세요!' }
                             </li>
                         )}
-                        <Link to='/guide'>
-                            <li className={pathname.startsWith('/guide') ? 'active' : ''}>
-                                가이드
-                            </li>
-                        </Link>
-                        <Link to='/quiz'>
-                            <li className={pathname.startsWith('/quiz') ? 'active' : ''}>
-                                퀴즈
-                            </li>
-                        </Link>
-                        <Link to='/challenge'> 
-                            <li className={pathname.startsWith('/chall') ? 'active' : ''}>
-                                챌린지
-                            </li>
-                        </Link>
+                        <li className={pathname.startsWith('/guide') ? 'active' : ''}>
+                            <Link to='/guide'>가이드</Link>
+                        </li>
+                        <li className={pathname.startsWith('/quiz') ? 'active' : ''}>
+                            <Link to='/quiz'>퀴즈</Link>
+                        </li>
+                        <li className={pathname.startsWith('/challenge') ? 'active' : ''}>
+                            <Link to='/challenge'>챌린지</Link>
+                        </li>
                     </ul>
                     <ul className='account'>
                         { user.loggedIn ? (
-                            <Link to='/logout'>
-                                <li className='material-icons'>
-                                    logout
-                                </li>
-                            </Link>
-                        ) : (
-                            <>
-                                <li className='material-icons link' onClick={() => setSignInVisible(true) }>
-                                login
-                                </li>
-                                <Link to='/signup'>
-                                    <li className='material-icons'>
-                                        person_add
-                                    </li>
-                                </Link>
-                            </>
-                        )}
+                            <li>
+                                <Link className="material-icons" to='/logout'>logout</Link>
+                            </li>
+                        ) : (<>
+                            <li>
+                                <i className='material-icons link' onClick={() => setSignInVisible(true) }>login</i>
+                            </li>
+
+                            <li>
+                                <Link className="material-icons" to='/signup'>person_add</Link>
+                            </li>
+                        </>)}
                         <li className={'mobileOnly material-icons link' + (expanded ? ' active' : '')} onClick={(e) => {
                             e.preventDefault();
                             setExpanded(!expanded);
