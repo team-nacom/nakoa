7c9369c4ca69b1e4aad2a07e44c461b82f975f08
김준원 <junis3@snu.ac.kr>
2021-04-07 10:35:49 +0900

    Fixed guide post not working

--- server/src/guide/poster.ts
+++ server/src/guide/poster.ts
@@ -25,6 +25,7 @@ export async function postOneGuide(guideObj: any) {
   else {
     const guide = new Guide(guideObj);
     
+    await guide.save();
     console.log(`Guide upload "${guide.name}" successful`);
 
     return guide;
