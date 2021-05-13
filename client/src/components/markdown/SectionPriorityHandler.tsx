89c7e935602b077bf5098c28602a3dcfad78f231
paulsohn <paulsohn@outlook.kr>
2021-05-13 23:16:23 +0000

    prevent multi-line proritized heading

--- client/src/components/markdown/SectionPriorityHandler.tsx
+++ client/src/components/markdown/SectionPriorityHandler.tsx
@@ -1,3 +1,4 @@
+import { notStrictEqual } from 'assert';
 import React, { Children } from 'react';
 
 import { Transformer, Plugin } from 'unified';
@@ -6,10 +7,14 @@ import { Node, Parent } from 'unist';
 const SectionPriorityHandler : Plugin = () => {
     const sectionPriorityHandler : Transformer = (tree, file) => {
         const root = tree as Parent;
-        const markers = ['A','B','C','D','E','+']; //+ for legacy.
-        const prio = [1,2,3,4,5,0];
+        const markers = ['A','B','C','D','E'];
+        const prio = [1,2,3,4,5];
 
-        for(const node of root.children){
+        for(var nodeno = 0; nodeno < root.children.length; ++nodeno){
+            //root.children.length may change during iteration.
+            
+            const node = root.children[nodeno]
+
             if(node.type === 'paragraph'
             && 'children' in node
             && Array.isArray(node.children) //no type guards for instances, seriously?
@@ -21,12 +26,31 @@ const SectionPriorityHandler : Plugin = () => {
                     const h = str.indexOf(marker);
                     
                     if(h !== -1 && [undefined,'#','##','###','####','#####','######'].indexOf(str.slice(0,h)) !== -1 ){ //indexOf result should be equal to h
+                        for(var j = 0; j< node.children.length; j++){
+                            const k = node.children[j].value.indexOf('\n');
+                            if(k !== -1){
+                                const N = {
+                                    type: 'paragraph',
+                                    children: node.children.slice(j+1)
+                                } as Parent;
+                                N.children.unshift({ type: 'text', value: node.children[j].value.slice(k+1) });
+
+                                node.children[j].value = node.children[j].value.slice(0,k);
+                                node.children.splice(j+1); //delete everything from j+1
+
+                                root.children.splice(nodeno+1, 0, N); //insert new node after current one
+
+                                break;
+                            }
+                        }
+
                         node.children[0].value = str.slice(h + marker.length).trimStart();
                         node.type = 'heading';
                         node.depth = h;
                         node.data = {
                             priority : prio[i]
                         }
+
                         break;
                     }
                 }
