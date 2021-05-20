92711ddfe0a72c5becf938134b7462486c936cd5
paulsohn <paulsohn@outlook.kr>
2021-05-20 15:41:08 +0000

    minor css improvement for editor

--- client/src/components/GuideEditor.tsx
+++ client/src/components/GuideEditor.tsx
@@ -85,12 +85,12 @@ function GuideEditor({ initialGuide, upload, author: _author, behavior } : Props
                 </div>
             </div>
 
-            <div className='adminForm'>
+            <div className=''>
                 <label> 가이드 제목 </label>
                 <input className='title' value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
 
-            <MarkdownEditor className='adminForm' body={ content } update={ (c) => setContent(c) } />
+            <MarkdownEditor className='' body={ content } update={ (c) => setContent(c) } />
 
             <div className='editorBottom'>
                 <div style={{flexGrow: 1, fontSize: '16px', lineHeight: '24px', margin: '30px 0px'}}>
