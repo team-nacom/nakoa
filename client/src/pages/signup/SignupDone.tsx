6758d2671a7fadcff68a67d6c3e19c7d1b89a878
김준원 <junis3@snu.ac.kr>
2021-08-05 20:21:33 +0900

    Added english translation to sign-in / sign-up UI

--- client/src/pages/signup/SignupDone.tsx
+++ client/src/pages/signup/SignupDone.tsx
@@ -1,6 +1,7 @@
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import React from 'react';
+import { useIntl } from 'react-intl';
 import { Redirect } from 'react-router-dom';
 
 interface Props {
@@ -9,15 +10,13 @@ interface Props {
 
 function SignUpDone({ location }: Props) {
     let verified = location.state.verified;
+    let intl = useIntl();
 
     if (verified === undefined) return <Redirect to='/' />
     else return (
         <>
             <Header />
-            { verified 
-                ? '이메일이 확인되었습니다! 나무컴퍼스의 회원이 되어주셔서 진심으로 감사드립니다. 이제 이메일과 비밀번호를 이용해 로그인하실 수 있습니다.' 
-                : '이메일을 확인하는 데에 실패했습니다... 이 페이지가 계속 뜨면 nacommanager@gmail.com으로 문의해주세요.'
-            }
+            { intl.formatMessage({ id: verified ? 'signup.success' : 'signup.failure' }) }
             <Footer />
         </>
     );
