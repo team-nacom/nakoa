a864e68dc7537f3ccdea75b5056deefa896cc412
Sunghyeon Jo <ainta@bucketplace.net>
2021-08-22 16:30:52 +0900

    add updateDate to guide

--- server/src/guide/poster.ts
+++ server/src/guide/poster.ts
@@ -71,7 +71,7 @@ export async function updateOneGuide(guideObj: any, index: number, user: UserDoc
   else {
     await updateGory(guideObj.index, guideObj.gory, guide.gory);
     
-    await Guide.findOneAndUpdate({ index: guideObj.index }, { $set: {content: guideObj.content, priority: guideObj.priority, name: guideObj.name} }, { runValidators: true }).exec();
+    await Guide.findOneAndUpdate({ index: guideObj.index }, { $set: {content: guideObj.content, priority: guideObj.priority, name: guideObj.name, updateDate: Date.now()} }, { runValidators: true }).exec();
 
     console.log(`Guide update "${guide.name}" successful`);
   }
