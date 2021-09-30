ae7e8a958ad1ee6aa632bee5e0ff710ba1175ccb
Younghun Roh <yhunroh@gmail.com>
2021-09-30 22:26:29 +0900

    Added isPublic to publish actions

--- client/src/components/GuideEditor.tsx
+++ client/src/components/GuideEditor.tsx
@@ -312,7 +312,7 @@ function GuideEditor({ initialGuide = {}, upload, behavior } : Props) {
                     async () => {
                         upload(
                             // TODO add tags
-                            { name, content, authors: authors.filter((s) => s.length > 0), tags: tags },
+                            { name, content, authors: authors.filter((s) => s.length > 0), tags: tags, isPublic: false },
                             setMessage
                         );
                     }
@@ -330,7 +330,7 @@ function GuideEditor({ initialGuide = {}, upload, behavior } : Props) {
                         let gory = goryIndex ?? (await postGory({ name: goryName, cate, guides: [] })).index;*/
                         
                         upload(
-                            { name, content, authors: authors.filter((s) => s.length > 0), tags: tags },
+                            { name, content, authors: authors.filter((s) => s.length > 0), tags: tags, isPublic: true },
                             setMessage
                         );
                     }
