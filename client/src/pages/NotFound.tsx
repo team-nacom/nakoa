ec979b3daeeffad66dc50826ca45e7aca6c36a98
paulsohn <paulsohn@outlook.kr>
2021-10-06 10:30:20 +0000

    scribbling

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -3,10 +3,23 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 import MarkdownRenderer from 'components/markdown/MarkdownRenderer';
 
-
+// import { ParentBubble, TextBubble } from 'components/nabubble/NaBubble';
 
 function NotFound() {
     let [message, setMessage] = React.useState('');
+
+    // let [namu, setNamu] = React.useState(new ParentBubble([
+    //     new TextBubble('AAAAAA'),
+    //     new TextBubble('BBB'),
+    // ]))
+
+    // React.useEffect(()=>{
+    //     const intv = setInterval(()=>{
+    //         console.log(namu.serialize(3))
+    //     },5000);
+
+    //     return () => clearInterval(intv);
+    // })
 
     fetch(process.env.PUBLIC_URL + '/404.md')
         .then(response => response.text())
@@ -19,6 +32,15 @@ function NotFound() {
             <MarkdownRenderer isManual={true}>
                 { message }
             </MarkdownRenderer>
+
+            {/* for testing: */}
+            {/* <React.Fragment>
+                { namu.render() }
+            </React.Fragment> */}
+
+            <textarea>
+                XXXX
+            </textarea>
             
             <Footer/>
         </>
