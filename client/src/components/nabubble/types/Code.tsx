f932b4ff02b38559c5aa6bbefeaa927e9e82edde
김준원 <junis3@snu.ac.kr>
2021-11-08 00:16:58 +0900

    Filled code bubble

--- client/src/components/nabubble/types/Code.tsx
+++ client/src/components/nabubble/types/Code.tsx
@@ -28,7 +28,7 @@ function RenderedCodeBubble(props: BubbleComponentProps){
     //     </Highlight>
     // ); 
     return (
-        <pre>
+        <pre className='codeBubble renderedCodeBubble'>
             <code>{ contents }</code>
         </pre>
     );
@@ -45,7 +45,7 @@ function PreviewCodeBubble(props: BubbleComponentProps){
     //     </Highlight>
     // ); 
     return (
-        <pre>
+        <pre className='codeBubble previewCodeBubble'>
             <code>{ contents }</code>
         </pre>
     );
