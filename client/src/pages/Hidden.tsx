b07f88938ea186c9ce51ea142155a7e58c2304c0
paulsohn <paulsohn@outlook.kr>
2022-02-01 05:59:39 +0000

    hotfix for compilation & some test

--- client/src/pages/Hidden.tsx
+++ client/src/pages/Hidden.tsx
@@ -2,29 +2,22 @@ import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
+import { Flat, findAdjacentId } from 'components/naflat/flat'
 import { FlatDisplayComponent, FlatEditorComponent } from 'components/naflat/component';
 
-
-function Hidden() {
-    // hidden bubble test page
-    return (
-        <>
-            <Header/>
-            <FlatEditorComponent
-                cellId = 'c0'
-                initialFlat = { {
-                    'c0' : {
-                        type: 'root',
-                        id: 'c0',
-                        childIds: ['c1','c2','c3'],
-                        value: '똑떨'
-                    },
-                    'c1' : {
-                        type: 'text',
-                        id: 'c1',
-                        parentId: 'c0',
-                        childIds: [],
-                        value:
+const initialFlat : Flat = {
+    'c0' : {
+        type: 'root',
+        id: 'c0',
+        childIds: ['c1','c2','c3'],
+        value: '똑떨'
+    },
+    'c1' : {
+        type: 'text',
+        id: 'c1',
+        parentId: 'c0',
+        childIds: [],
+        value:
 `# 제목
 
 으아아아앙
@@ -35,13 +28,13 @@ $$
 x^2 + y^2 = z^2
 $$
 수식입력도 좀 해보고`
-                    },
-                    'c2' : {
-                        type: 'text',
-                        id: 'c2',
-                        parentId: 'c0',
-                        childIds: ['c22','c23'],
-                        value:
+    },
+    'c2' : {
+        type: 'text',
+        id: 'c2',
+        parentId: 'c0',
+        childIds: ['c22','c23'],
+        value:
 `# 또다른 제목
 
 꺄르르르륵
@@ -49,13 +42,13 @@ $$
 ## 또다른 부제목
 
 > 아아, 이것은 인용구라는 것이다.`
-                    },
-                    'c3' : {
-                        type: 'text',
-                        id: 'c3',
-                        parentId: 'c0',
-                        childIds: [],
-                        value:
+    },
+    'c3' : {
+        type: 'text',
+        id: 'c3',
+        parentId: 'c0',
+        childIds: [],
+        value:
 `# 세번째 제목
 
 그롸롸롸롸
@@ -65,22 +58,36 @@ $$
 @@@
 
 나컴-exclusive한 것도 표시는 잘 되긴 하는데.`
-                    },
-                    'c22' : {
-                        type: 'code',
-                        id: 'c22',
-                        parentId: 'c2',
-                        childIds: [],
-                        value: `console.log('Hello, World!')`
-                    },
-                    'c23' : {
-                        type: 'math',
-                        id: 'c23',
-                        parentId: 'c2',
-                        childIds: [],
-                        value: `p_\\mu p^\\mu = -m^2 \\quad (-+++)`
-                    }
-                } }
+    },
+    'c22' : {
+        type: 'code',
+        id: 'c22',
+        parentId: 'c2',
+        childIds: [],
+        value: `console.log('Hello, World!')`
+    },
+    'c23' : {
+        type: 'math',
+        id: 'c23',
+        parentId: 'c2',
+        childIds: [],
+        value: `p_\\mu p^\\mu = -m^2 \\quad (-+++)`
+    }
+}
+
+function Hidden() {
+    // hidden bubble test page
+
+    for(var key in Object.keys(initialFlat)){
+        console.log(key, findAdjacentId(initialFlat,key,1), findAdjacentId(initialFlat,key,-1));
+    }
+
+    return (
+        <>
+            <Header/>
+            <FlatEditorComponent
+                cellId = 'c0'
+                initialFlat = { initialFlat }
             />
 
             <Footer/>
