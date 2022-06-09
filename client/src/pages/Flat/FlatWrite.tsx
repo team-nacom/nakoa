1650b541ac10b25023716d3f856ef2616574384c
Younghun Roh <yhunroh@gmail.com>
2022-06-09 23:59:32 +0900

    Fixed cloning

--- client/src/pages/Flat/FlatWrite.tsx
+++ client/src/pages/Flat/FlatWrite.tsx
@@ -1,21 +1,38 @@
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 
-import { postFlat, FlatUploadItem } from 'etc/api/flat';
+import { postFlat, FlatUploadItem, FlatItem } from 'etc/api/flat';
 import React, {useCallback, useEffect, useRef} from 'react';
 import { Redirect, useLocation } from 'react-router';
 import { Flat } from 'components/naflat/flat';
 import { FlatItemMetadata, FlatEditor } from 'components/editor/FlatEditor';
 import { localStorageKeys } from 'etc/consts';
 
+interface State {
+    copySourceFlat?: FlatItem
+}
 
 function FlatWrite() {
     let [redirectTo, setRedirectTo] = React.useState<string>();
 
+    let location = useLocation<State>();
+    let clonedFlat = location.state?.copySourceFlat;
+    
     let storedFlatDraftString = localStorage.getItem(localStorageKeys.flatDraft);
     let storedFlatDraft = storedFlatDraftString ? JSON.parse(storedFlatDraftString) : null;
     let storedMetadataDraftString = localStorage.getItem(localStorageKeys.metadataDraft);
     let storedMetadataDraft = storedMetadataDraftString ? JSON.parse(storedMetadataDraftString) : null;
+
+    let initialMetadata = { title: '', author: '' };
+    let initialFlat = undefined;
+    if (clonedFlat && clonedFlat.content) {
+        initialMetadata = { title: clonedFlat.title, author: clonedFlat.author };
+        initialFlat = JSON.parse(clonedFlat.content);
+    }
+    else {
+        initialMetadata = storedMetadataDraft ?? initialMetadata;
+        initialFlat = storedFlatDraft ?? initialFlat;
+    }
 
     let upload = (metadata: FlatItemMetadata, flat: Flat) => {
         postFlat(metadata, flat).then(({success, index}) => {
@@ -35,8 +52,8 @@ function FlatWrite() {
             <div id='content'>
                 <FlatEditor
                     upload={upload}
-                    metadata={ storedMetadataDraft ?? { title: '', author: '' } }
-                    initialFlat={storedFlatDraft ?? undefined}
+                    metadata={initialMetadata}
+                    initialFlat={initialFlat}
                 />
             </div>
             <Footer/>
