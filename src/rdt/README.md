ee83245d0dcb8d137e831f8cd5a55e06876a9514
Taeseung Sohn <paulsohn@outlook.kr>
2022-08-23 14:19:55 +0000

    import react-dnd-treeview example

---
+++ src/rdt/README.md
@@ -0,0 +1,3 @@
+This example is forked from:
+
+https://codesandbox.io/s/custom-drag-preview-ts-ibvb07?from-embed=&runonclick=1
---
+++ src/rdt/RdtApp.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import {
+  Tree,
+  NodeModel,
+  DragLayerMonitorProps,
+  MultiBackend,
+  getBackendOptions,
+  DndProvider
+} from "@minoru/react-dnd-treeview";
+import { CustomData } from "./types";
+import { CustomNode } from "./CustomNode";
+import { CustomDragPreview } from "./CustomDragPreview";
+import { theme } from "./theme";
+import styles from "./App.module.css";
+import SampleData from "./sample_data.json";
+
+function App() {
+  const [treeData, setTreeData] = useState<NodeModel<CustomData>[]>(SampleData);
+  const handleDrop = (newTree: NodeModel<CustomData>[]) => setTreeData(newTree);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+        <div className={styles.app}>
+          <Tree<CustomData>
+            tree={treeData}
+            rootId={0}
+            render={(
+              node: NodeModel<CustomData>,
+              { depth, isOpen, onToggle }
+            ) => (
+              <CustomNode
+                node={node}
+                depth={depth}
+                isOpen={isOpen}
+                onToggle={onToggle}
+              />
+            )}
+            dragPreviewRender={(
+              monitorProps: DragLayerMonitorProps<CustomData>
+            ) => <CustomDragPreview monitorProps={monitorProps} />}
+            onDrop={handleDrop}
+            classes={{
+              root: styles.treeRoot,
+              draggingSource: styles.draggingSource,
+              dropTarget: styles.dropTarget
+            }}
+          />
+        </div>
+      </DndProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
