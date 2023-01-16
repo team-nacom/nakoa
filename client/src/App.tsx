010593c4185d2db067cbe89150118127f6cd628b
Taeseung Sohn <paulsohn@outlook.kr>
2023-01-16 11:50:16 +0000

    add layout folder for future use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,13 +9,15 @@ import ReactGA from 'react-ga';
 import { useLocale } from '#/store/locale'
 import { localeMessages } from '#/locale'
 
-import Header from '#/components/Header';
-import Footer from '#/components/Footer';
+import Header from '#/layout/Header';
+import Footer from '#/layout/Footer';
 
-import Hidden from '#/pages/Hidden'
-import Write from '#/pages/Write'
-import NotFound from '#/pages/NotFound'
-import About from '#/pages/About'
+import Main from '#/pages/Main';
+import Hidden from '#/pages/Hidden';
+import WriteClassic from './pages/WriteClassic';
+import WriteCell from '#/pages/WriteCell';
+import NotFound from '#/pages/NotFound';
+import About from '#/pages/About';
 
 function App() {
     const { locale } = useLocale()
@@ -33,10 +35,14 @@ function App() {
                 <Header />
                 <div id='content'>
                     <Switch>
-                        <Redirect exact path='/' to='/hidden' />
-                        <Route exact path='/hidden' component={Hidden} />
-                        <Route exact path='/write' component={Write} />
+                        <Redirect exact path='/' to='/main' />
+                        <Route exact path='/main' component={Main} />
                         <Route exact path='/about' component={About}/>
+
+                        <Route exact path='/write-classic' component={WriteClassic} />
+                        <Route exact path='/write-cell' component={WriteCell} />
+
+                        <Route exact path='/hidden' component={Hidden} />
                         <Route component={NotFound}/>
                     </Switch>
                 </div>
