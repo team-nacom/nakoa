18cd8a1918b2f0d828f3acb19c8157df67187b4f
TAMREF <sluggeryck@naver.com>
2023-05-07 17:30:45 +0900

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,7 +4,7 @@ pm2 stop all
 pm2 delete all
 
 cd ~/nakoa/server
-pm2 start --node-args="-r dotenv/config" build/index.js --name server
+pm2 start --node-args="-r dotenv/config -r tsconfig-paths/register" build/index.js --name server
 
 cd ~/nakoa/client
 pm2 serve --spa build 3000 --name client
